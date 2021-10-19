9ece6c2760bcf7e4a83c71f6ab884510f5186a79
Danylo Ivzhenko <ivzhenkodanil@gmail.com>
2021-10-19 21:50:27 +0300

    Coffee Machine Project - Stage 2

--- src/coffeemachine/CoffeeMachine.java
+++ src/coffeemachine/CoffeeMachine.java
@@ -1,5 +1,16 @@
 package coffeemachine;
 
-public class CoffeeMachine {
+public class CoffeeMachine
+{
+    static void countCoffee(int cups)
+    {
+        int coffeeBeans = 15;
+        int milk = 50;
+        int water = 200;
 
+        System.out.println("For " + cups + " of coffee you will need:" );
+        System.out.println(water * cups + " ml of water");
+        System.out.println(milk * cups + " ml of milk");
+        System.out.println(coffeeBeans * cups + " g of coffee beans");
+    }
 }
--- src/coffeemachine/Main.java
+++ src/coffeemachine/Main.java
@@ -1,14 +1,25 @@
 package coffeemachine;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
+
+import static coffeemachine.CoffeeMachine.countCoffee;
+
+public class Main
+{
+    static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args)
+    {
         //Stage 1
+        /*
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
         System.out.println("Mixing boiled water with crushed coffee beans");
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        System.out.println("Coffee is ready!"); */
+
+        System.out.println("Write how many cups of coffee you will need:");
+        countCoffee(scanner.nextInt());
     }
 }
