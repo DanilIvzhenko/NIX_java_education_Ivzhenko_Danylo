3ee003a431b59eb7d7e43155c8010cd5ee387b18
Danylo Ivzhenko <ivzhenkodanil@gmail.com>
2021-10-20 17:32:31 +0300

    Coffee Machine Project - Stage 3

--- src/coffeemachine/CoffeeMachine.java
+++ src/coffeemachine/CoffeeMachine.java
@@ -1,5 +1,7 @@
 package coffeemachine;
 
+import static java.lang.Math.min;
+
 public class CoffeeMachine
 {
     static void countCoffee(int cups)
@@ -12,5 +14,25 @@ public class CoffeeMachine
         System.out.println(water * cups + " ml of water");
         System.out.println(milk * cups + " ml of milk");
         System.out.println(coffeeBeans * cups + " g of coffee beans");
+    }
+
+    static void countCups(int water, int milk, int coffeeBeans, int cups)
+    {
+        //200 ml water, 50 ml milk, 15g coffee
+
+        int resCups = Math.min(water / 200, Math.min(milk / 50, coffeeBeans / 15));
+
+        if (cups == resCups)
+        {
+            System.out.println("Yes, i can make that amount of coffee");
+        }
+        else if (resCups > cups)
+        {
+            System.out.println("Yes, I can make that amount of coffee and even " + resCups + " more than that");
+        }
+        else
+        {
+            System.out.println("No, i can make only " + (resCups) + " cups of coffee");
+        }
     }
 }
--- src/coffeemachine/Main.java
+++ src/coffeemachine/Main.java
@@ -3,12 +3,14 @@ package coffeemachine;
 import java.util.Scanner;
 
 import static coffeemachine.CoffeeMachine.countCoffee;
+import static coffeemachine.CoffeeMachine.countCups;
 
 public class Main
 {
     static final Scanner scanner = new Scanner(System.in);
     public static void main(String[] args)
     {
+
         //Stage 1
         /*
         System.out.println("Starting to make a coffee");
@@ -18,8 +20,15 @@ public class Main
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!"); */
-
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int water = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int milk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int coffeeBeans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need:");
-        countCoffee(scanner.nextInt());
+        int cups = scanner.nextInt();
+        countCups(water, milk, coffeeBeans, cups);
+
     }
 }
