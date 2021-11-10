3db3ebdbfe05e1f1f0b208bd7d8f53ac8d228f4f
Danylo Ivzhenko <ivzhenkodanil@gmail.com>
2021-11-10 18:36:03 +0200

    Tictactoe Project - Stage 1

--- src/Tictactoe/MainTictactoe.java
+++ src/Tictactoe/MainTictactoe.java
@@ -1,6 +1,8 @@
 package Tictactoe;
 public class MainTictactoe {
     public static void main(String[] args) {
-
+        System.out.println("X O X");
+        System.out.println("O X O");
+        System.out.println("X X O");
     }
 }
