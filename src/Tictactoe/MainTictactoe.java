dc0e3b18cc53ec6622277654ddc609db7bd37743
Danylo Ivzhenko <ivzhenkodanil@gmail.com>
2021-11-10 18:56:22 +0200

    Tictactoe Project - Stage 2

--- src/Tictactoe/MainTictactoe.java
+++ src/Tictactoe/MainTictactoe.java
@@ -1,8 +1,24 @@
 package Tictactoe;
+
+import java.util.Scanner;
+
 public class MainTictactoe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        //System.out.println("X O X");
+        //System.out.println("O X O");
+        //System.out.println("X X O");
+        //XOXXOOXOX
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells:");
+        String line = scanner.nextLine();
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%s ", line.charAt(3 * i + j));
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
